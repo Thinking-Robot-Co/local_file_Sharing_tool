a92d44ac0d25618f63530982401c1afa7cf07344
Thinking-Robot-Co <mythinkingrobot@gmail.com>
2025-03-12 16:39:16 +0530

    Initial commit: Set up project structure

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // For now, simply log a message to verify that the file is loaded
+    console.log("Script loaded successfully!");
+    
+    // You will later initialize PeerJS here
+  });
+  
